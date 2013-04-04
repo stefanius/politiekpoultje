edf832303e603bfaf0330047c725f373e6d886a5
stefanius <sgrootveld@gmail.com>
2013-04-04 19:56:21 +0200

    Changed baseurl

--- cfg/cfg.application.php
+++ cfg/cfg.application.php
@@ -2,7 +2,7 @@
 define("USE_CORE_PACKAGE_BOOTSTRAP", true);
 define("DO_DEBUGGING", TRUE); //set debugmode
 define("APP_NAME", "Politiekpoultje.nl"); //name of the application
-define("URL_BASE", "/politiekpoultje/"); //Base url
+define("URL_BASE", "/"); //Base url
 //define("URL_BASE", "/politiekpoultje/"); //Base url
 define("ERROR_THROW", TRUE); //defining if we want to convert php errors into exceptions.
 define("DEFAULT_PW_HASH", 'joomla_classic'); //defining if we want to convert php errors into exceptions.
