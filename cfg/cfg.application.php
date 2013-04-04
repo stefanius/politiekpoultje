f8f04a2290e3fb5241ab3cdcc16809bfa800a9a0
stefanius <sgrootveld@gmail.com>
2013-04-04 21:35:06 +0200

    Added htracces

--- cfg/cfg.application.php
+++ cfg/cfg.application.php
@@ -1,8 +1,8 @@
 <?php
 define("USE_CORE_PACKAGE_BOOTSTRAP", true);
 define("DO_DEBUGGING", TRUE); //set debugmode
-define("APP_NAME", "Politiekpoultje.nlll"); //name of the application
-define("URL_BASE", "/"); //Base url
+define("APP_NAME", "Politiekpoultje.nl"); //name of the application
+define("URL_BASE", "/politiekpoultje/"); //Base url
 //define("URL_BASE", "/politiekpoultje/"); //Base url
 define("ERROR_THROW", TRUE); //defining if we want to convert php errors into exceptions.
 define("DEFAULT_PW_HASH", 'joomla_classic'); //defining if we want to convert php errors into exceptions.
